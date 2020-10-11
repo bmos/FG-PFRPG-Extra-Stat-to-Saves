59f415f349b9c2d612a3c84841e7c54d3ba45b6b
Wil Thieme <wil.thieme@protonmail.com>
2020-10-11 16:37:03 -0400

    only show message about saves to the appropriate gm/user

--- scripts/prep_paladin_level2.lua
+++ scripts/prep_paladin_level2.lua
@@ -3,25 +3,35 @@
 --
 
 function onInit()
-	if User.isHost() then DB.addHandler(DB.getPath('charsheet.*.classlevel'), 'onUpdate', prepPaladin) end
+	DB.addHandler(DB.getPath('charsheet.*.classlevel'), 'onUpdate', prepPaladin)
 end
 
----	This function auto-sets the second ability stat for each save to Charisma for lvl2+ Paladins.
+---	This function auto-sets the second ability stat for each unmodified save to charisma for lvl 2 and up paladins.
 function prepPaladin(nodeClassLevel)
-	local nodeChar = nodeClassLevel.getChild('..')
-	local nPalLvl = DB.getValue(CharManager.getClassNode(nodeChar, 'Paladin'), "level")
+	if DB.isOwner(nodeClassLevel) then
+		local nodeChar = nodeClassLevel.getChild('..')
+		local nPalLvl = DB.getValue(CharManager.getClassNode(nodeChar, 'Paladin'), "level")
 
-	if nPalLvl and nPalLvl >= 2 then
-		local sFort2Stat = DB.getValue(nodeChar, 'saves.fortitude.ability2')
-		local sRef2Stat = DB.getValue(nodeChar, 'saves.reflex.ability2')
-		local sWill2Stat = DB.getValue(nodeChar, 'saves.will.ability2')
+		if nPalLvl and nPalLvl >= 2 then -- if PC is at least a 2nd level paladin
+			local sFort2Stat = DB.getValue(nodeChar, 'saves.fortitude.ability2')
+			local sRef2Stat = DB.getValue(nodeChar, 'saves.reflex.ability2')
+			local sWill2Stat = DB.getValue(nodeChar, 'saves.will.ability2')
+			
+			local bGMOnlyConnected = nil -- check if GM is leveling-up for players
+			if table.getn(User.getActiveUsers()) == 0 and User.isHost() then bGMOnlyConnected = true end
+			
+			local bStatChanged = nil -- check if any of the ability2 fields are still at default values
+			if not sFort2Stat or not sRef2Stat or not sWill2Stat or sFort2Stat == '' or sRef2Stat == '' or sWill2Stat == '' then bStatChanged = true end
 
-		if not sFort2Stat or sFort2Stat == '' then DB.setValue(nodeChar, 'saves.fortitude.ability2', 'string', 'charisma') end
-		if not sRef2Stat or sRef2Stat == '' then DB.setValue(nodeChar, 'saves.reflex.ability2', 'string', 'charisma') end
-		if not sWill2Stat or sWill2Stat == '' then DB.setValue(nodeChar, 'saves.will.ability2', 'string', 'charisma') end
-		
-		local sFormat = Interface.getString("char_message_paladinsecondsave");
-		local sMsg = string.format(sFormat, DB.getValue(nodeChar, "name", ""));
-		ChatManager.SystemMessage(sMsg);
+			if bStatChanged and (not User.isHost() or bGMOnlyConnected) then -- if any ability2 are at default values, set them to charisma
+				local sFormat = Interface.getString('char_message_paladinsecondsave') -- add mention of this change to the level-up messaging
+				local sMsg = string.format(sFormat, DB.getValue(nodeChar, "name", ''))
+				ChatManager.SystemMessage(sMsg);
+				
+				if not sFort2Stat or sFort2Stat == '' then DB.setValue(nodeChar, 'saves.fortitude.ability2', 'string', 'charisma') end
+				if not sRef2Stat or sRef2Stat == '' then DB.setValue(nodeChar, 'saves.reflex.ability2', 'string', 'charisma') end
+				if not sWill2Stat or sWill2Stat == '' then DB.setValue(nodeChar, 'saves.will.ability2', 'string', 'charisma') end
+			end
+		end
 	end
 end
