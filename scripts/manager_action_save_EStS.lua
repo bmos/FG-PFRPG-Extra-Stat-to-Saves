f140731d4bef985f1eec27f0008d869540e2a411
Wil Thieme <wil.thieme@protonmail.com>
2022-03-24 20:59:04 -0400

    line length updates and formatting

--- scripts/manager_action_save_EStS.lua
+++ scripts/manager_action_save_EStS.lua
@@ -1,106 +1,112 @@
 --
 -- Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-
 local getRoll_old = nil
 local modSave_old = nil
 
 local function getRoll_new(rActor, sSave, ...)
-	local rRoll = getRoll_old(rActor, sSave, ...); -- inheret output of previously-loaded getRoll function
-	-- bmos adding second save stat
-	local sAbility2
-	if rActor then
-		local nodeCT = ActorManager.getCTNode(rActor);
-		if ActorManager.isPC(rActor) then
-			local nodePC = ActorManager.getCreatureNode(rActor);
-			rRoll.nMod = DB.getValue(nodePC, "saves." .. sSave .. ".total", 0);
-			sAbility2 = DB.getValue(nodePC, "saves." .. sSave .. ".ability2", "");
-		elseif nodeCT then
-			rRoll.nMod = DB.getValue(nodeCT, sSave .. "save", 0);
-		end
-	end
-	-- bmos adding second save ability
+    local rRoll = getRoll_old(rActor, sSave, ...); -- inheret output of previously-loaded getRoll function
+    -- bmos adding second save stat
+    local sAbility2
+    if rActor then
+        local nodeCT = ActorManager.getCTNode(rActor);
+        if ActorManager.isPC(rActor) then
+            local nodePC = ActorManager.getCreatureNode(rActor);
+            rRoll.nMod = DB.getValue(nodePC, "saves." .. sSave .. ".total", 0);
+            sAbility2 =
+                DB.getValue(nodePC, "saves." .. sSave .. ".ability2", "");
+        elseif nodeCT then
+            rRoll.nMod = DB.getValue(nodeCT, sSave .. "save", 0);
+        end
+    end
+    -- bmos adding second save ability
 
-	-- bmos adding extra save mod to roll
-	if sAbility2 and sAbility2 ~= "" then
-		local sAbilityEffect2 = DataCommon.ability_ltos[sAbility2];
-		if sAbilityEffect2 then
-			rRoll.sDesc = rRoll.sDesc .. " [EXTRA MOD: " .. sAbilityEffect2 .. "]";
-		end
-	end
-	-- end bmos adding extra save mod to roll
+    -- bmos adding extra save mod to roll
+    if sAbility2 and sAbility2 ~= "" then
+        local sAbilityEffect2 = DataCommon.ability_ltos[sAbility2];
+        if sAbilityEffect2 then
+            rRoll.sDesc = rRoll.sDesc .. " [EXTRA MOD: " .. sAbilityEffect2 ..
+                              "]";
+        end
+    end
+    -- end bmos adding extra save mod to roll
 
-	return rRoll;
+    return rRoll;
 end
 
 -- It seems I must override this function completely as it does not return data
 -- I have included checks to ensure compatibility with Kelrugem's Save Versus Tags
 -- Includes current 3.5E ruleset code as of 2021-08-01
 local function modSave_new(rSource, rTarget, rRoll, ...)
-	modSave_old(rSource, rTarget, rRoll, ...)
-	local aAddDesc = {};
-	local aAddDice = {};
-	local nAddMod = 0;
+    modSave_old(rSource, rTarget, rRoll, ...)
+    local aAddDesc = {};
+    local aAddDice = {};
+    local nAddMod = 0;
 
-	if rSource then
-		-- Determine ability used
-		local sActionStat2 = nil; -- bmos adding second save stat
-		local sModStat2 = string.match(rRoll.sDesc, "%[EXTRA MOD: (%w+)%]"); -- bmos adding second save stat
-		if sModStat2 then -- bmos adding second save stat
-			sActionStat2 = DataCommon.ability_stol[sModStat2];
-		end
-		-- end bmos adding second save stat
+    if rSource then
+        -- Determine ability used
+        local sActionStat2 = nil; -- bmos adding second save stat
+        local sModStat2 = string.match(rRoll.sDesc, "%[EXTRA MOD: (%w+)%]"); -- bmos adding second save stat
+        if sModStat2 then -- bmos adding second save stat
+            sActionStat2 = DataCommon.ability_stol[sModStat2];
+        end
+        -- end bmos adding second save stat
 
-		local bEffects;
-		-- bmos adding effects for second save stat
-		local nBonusStat2, nBonusEffects2 = ActorManager35E.getAbilityEffectsBonus(rSource, sActionStat2);
-		if nBonusEffects2 > 0 then
-			bEffects = true;
-			nAddMod = nAddMod + nBonusStat2;
-		end
-		-- end bmos adding effects for second save stat
+        local bEffects;
+        -- bmos adding effects for second save stat
+        local nBonusStat2, nBonusEffects2 =
+            ActorManager35E.getAbilityEffectsBonus(rSource, sActionStat2);
+        if nBonusEffects2 > 0 then
+            bEffects = true;
+            nAddMod = nAddMod + nBonusStat2;
+        end
+        -- end bmos adding effects for second save stat
 
-		-- If effects, then add them to the in-chat roll description
-		if bEffects then
-			local sEffects;
-			local sMod = DiceManager.convertDiceToString(aAddDice, nAddMod, true);
-			if sMod ~= "" then
-				sEffects = "[SECOND MOD " .. Interface.getString("effects_tag") .. " " .. sMod .. "]";
-			else
-				sEffects = "[SECOND MOD " .. Interface.getString("effects_tag") .. "]";
-			end
-			table.insert(aAddDesc, sEffects);
-		end
-	end
+        -- If effects, then add them to the in-chat roll description
+        if bEffects then
+            local sEffects;
+            local sMod =
+                DiceManager.convertDiceToString(aAddDice, nAddMod, true);
+            if sMod ~= "" then
+                sEffects =
+                    "[SECOND MOD " .. Interface.getString("effects_tag") .. " " ..
+                        sMod .. "]";
+            else
+                sEffects =
+                    "[SECOND MOD " .. Interface.getString("effects_tag") .. "]";
+            end
+            table.insert(aAddDesc, sEffects);
+        end
+    end
 
-	if #aAddDesc > 0 then
-		rRoll.sDesc = rRoll.sDesc .. " " .. table.concat(aAddDesc, " ");
-	end
-	for _,vDie in ipairs(aAddDice) do
-		if vDie:sub(1,1) == "-" then
-			table.insert(rRoll.aDice, "-p" .. vDie:sub(3));
-		else
-			table.insert(rRoll.aDice, "p" .. vDie:sub(2));
-		end
-	end
-	rRoll.nMod = rRoll.nMod + nAddMod;
+    if #aAddDesc > 0 then
+        rRoll.sDesc = rRoll.sDesc .. " " .. table.concat(aAddDesc, " ");
+    end
+    for _, vDie in ipairs(aAddDice) do
+        if vDie:sub(1, 1) == "-" then
+            table.insert(rRoll.aDice, "-p" .. vDie:sub(3));
+        else
+            table.insert(rRoll.aDice, "p" .. vDie:sub(2));
+        end
+    end
+    rRoll.nMod = rRoll.nMod + nAddMod;
 end
 
 -- Function Overrides
 function onInit()
-	getRoll_old = ActionSave.getRoll;
-	ActionSave.getRoll = getRoll_new;
-	modSave_old = ActionSave.modSave;
-	ActionSave.modSave = modSave_new;
+    getRoll_old = ActionSave.getRoll;
+    ActionSave.getRoll = getRoll_new;
+    modSave_old = ActionSave.modSave;
+    ActionSave.modSave = modSave_new;
 
-	ActionsManager.unregisterModHandler("save");
-	ActionsManager.registerModHandler("save", modSave_new);
+    ActionsManager.unregisterModHandler("save");
+    ActionsManager.registerModHandler("save", modSave_new);
 end
 
 function onClose()
-	ActionSave.getRoll = getRoll_old;
-	ActionSave.modSave = modSave_old;
+    ActionSave.getRoll = getRoll_old;
+    ActionSave.modSave = modSave_old;
 
-	ActionsManager.unregisterModHandler("save");
-	ActionsManager.registerModHandler("save", ActionSave.modSave);
+    ActionsManager.unregisterModHandler("save");
+    ActionsManager.registerModHandler("save", ActionSave.modSave);
 end
--- scripts/prep_paladin_level2.lua
+++ scripts/prep_paladin_level2.lua
@@ -1,38 +1,55 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
-
 ---	This function auto-sets the second ability stat for each unmodified save to charisma for lvl 2 and up paladins.
 local function prepPaladin(nodeClassLevel)
-	if DB.isOwner(nodeClassLevel) then
-		local nodeChar = nodeClassLevel.getChild('..')
-		local nPalLvl = DB.getValue(CharManager.getClassNode(nodeChar, 'Paladin'), "level")
+    if DB.isOwner(nodeClassLevel) then
+        local nodeChar = nodeClassLevel.getChild('..')
+        local nPalLvl = DB.getValue(
+                            CharManager.getClassNode(nodeChar, 'Paladin'),
+                            "level")
 
-		if nPalLvl and nPalLvl >= 2 then -- if PC is at least a 2nd level paladin
-			local sFort2Stat = DB.getValue(nodeChar, 'saves.fortitude.ability2')
-			local sRef2Stat = DB.getValue(nodeChar, 'saves.reflex.ability2')
-			local sWill2Stat = DB.getValue(nodeChar, 'saves.will.ability2')
+        if nPalLvl and nPalLvl >= 2 then -- if PC is at least a 2nd level paladin
+            local sFort2Stat = DB.getValue(nodeChar, 'saves.fortitude.ability2')
+            local sRef2Stat = DB.getValue(nodeChar, 'saves.reflex.ability2')
+            local sWill2Stat = DB.getValue(nodeChar, 'saves.will.ability2')
 
-			local bGMOnlyConnected = nil -- check if GM is leveling-up for players
-			-- luacheck: globals table
-			if table.getn(User.getActiveUsers()) == 0 and Session.IsHost then bGMOnlyConnected = true end
+            local bGMOnlyConnected = nil -- check if GM is leveling-up for players
+            -- luacheck: globals table
+            if table.getn(User.getActiveUsers()) == 0 and Session.IsHost then
+                bGMOnlyConnected = true
+            end
 
-			local bStatChanged = nil -- check if any of the ability2 fields are still at default values
-			if not sFort2Stat or not sRef2Stat or not sWill2Stat or sFort2Stat == '' or sRef2Stat == '' or sWill2Stat == '' then bStatChanged = true end
+            local bStatChanged = nil -- check if any of the ability2 fields are still at default values
+            if not sFort2Stat or not sRef2Stat or not sWill2Stat or sFort2Stat ==
+                '' or sRef2Stat == '' or sWill2Stat == '' then
+                bStatChanged = true
+            end
 
-			if bStatChanged and (not Session.IsHost or bGMOnlyConnected) then -- if any ability2 are at default values, set them to charisma
-				local sFormat = Interface.getString('char_message_paladinsecondsave') -- add mention of this change to the level-up messaging
-				local sMsg = string.format(sFormat, DB.getValue(nodeChar, "name", ''))
-				ChatManager.SystemMessage(sMsg);
+            if bStatChanged and (not Session.IsHost or bGMOnlyConnected) then -- if any ability2 are at default values, set them to charisma
+                local sFormat = Interface.getString(
+                                    'char_message_paladinsecondsave') -- add mention of this change to the level-up messaging
+                local sMsg = string.format(sFormat,
+                                           DB.getValue(nodeChar, "name", ''))
+                ChatManager.SystemMessage(sMsg);
 
-				if not sFort2Stat or sFort2Stat == '' then DB.setValue(nodeChar, 'saves.fortitude.ability2', 'string', 'charisma') end
-				if not sRef2Stat or sRef2Stat == '' then DB.setValue(nodeChar, 'saves.reflex.ability2', 'string', 'charisma') end
-				if not sWill2Stat or sWill2Stat == '' then DB.setValue(nodeChar, 'saves.will.ability2', 'string', 'charisma') end
-			end
-		end
-	end
+                if not sFort2Stat or sFort2Stat == '' then
+                    DB.setValue(nodeChar, 'saves.fortitude.ability2', 'string',
+                                'charisma')
+                end
+                if not sRef2Stat or sRef2Stat == '' then
+                    DB.setValue(nodeChar, 'saves.reflex.ability2', 'string',
+                                'charisma')
+                end
+                if not sWill2Stat or sWill2Stat == '' then
+                    DB.setValue(nodeChar, 'saves.will.ability2', 'string',
+                                'charisma')
+                end
+            end
+        end
+    end
 end
 
 function onInit()
-	DB.addHandler(DB.getPath('charsheet.*.classlevel'), 'onUpdate', prepPaladin)
+    DB.addHandler(DB.getPath('charsheet.*.classlevel'), 'onUpdate', prepPaladin)
 end
